c2e9d949f3b4432a432a422c13116e92524dc729
olga <olgaa.yatsuk@gmail.com>
2018-09-14 11:06:46 +0300

    fix;

--- scripts/app.js
+++ scripts/app.js
@@ -13793,6 +13793,52 @@ sliders.filter(function (slider) {
   return slider.isEnabled && new Slider(slider.selector, slider.config);
 });
 
+jquery$1('select').each(function () {
+  var $this = jquery$1(this),
+      numberOfOptions = jquery$1(this).children('option').length;
+
+  $this.addClass('select-hidden');
+  $this.wrap('<div class="select"></div>');
+  $this.after('<div class="select-styled"></div>');
+
+  var $styledSelect = $this.next('div.select-styled');
+  $styledSelect.text($this.children('option').eq(0).text());
+
+  var $list = jquery$1('<ul />', {
+    'class': 'select-options'
+  }).insertAfter($styledSelect);
+
+  for (var i = 0; i < numberOfOptions; i++) {
+    jquery$1('<li />', {
+      text: $this.children('option').eq(i).text(),
+      rel: $this.children('option').eq(i).val()
+    }).appendTo($list);
+  }
+
+  var $listItems = $list.children('li');
+
+  $styledSelect.click(function (e) {
+    e.stopPropagation();
+    jquery$1('div.select-styled.active').not(this).each(function () {
+      jquery$1(this).removeClass('active').next('ul.select-options').hide();
+    });
+    jquery$1(this).toggleClass('active').next('ul.select-options').fadeToggle(120);
+  });
+
+  $listItems.click(function (e) {
+    e.stopPropagation();
+    $styledSelect.text(jquery$1(this).text()).removeClass('active');
+    $this.val(jquery$1(this).attr('rel'));
+    $list.hide();
+    //console.log($this.val());
+  });
+
+  jquery$1(document).click(function () {
+    $styledSelect.removeClass('active');
+    $list.hide();
+  });
+});
+
 var Scroll = function Scroll() {
   var $root = jquery$1('body,html');
   var $menu = jquery$1('.js-nav-about');
@@ -13934,50 +13980,4 @@ Modal();
 Menu();
 Tabs();
 Policy();
-
-jquery$1('select').each(function () {
-  var $this = jquery$1(this),
-      numberOfOptions = jquery$1(this).children('option').length;
-
-  $this.addClass('select-hidden');
-  $this.wrap('<div class="select"></div>');
-  $this.after('<div class="select-styled"></div>');
-
-  var $styledSelect = $this.next('div.select-styled');
-  $styledSelect.text($this.children('option').eq(0).text());
-
-  var $list = jquery$1('<ul />', {
-    'class': 'select-options'
-  }).insertAfter($styledSelect);
-
-  for (var i = 0; i < numberOfOptions; i++) {
-    jquery$1('<li />', {
-      text: $this.children('option').eq(i).text(),
-      rel: $this.children('option').eq(i).val()
-    }).appendTo($list);
-  }
-
-  var $listItems = $list.children('li');
-
-  $styledSelect.click(function (e) {
-    e.stopPropagation();
-    jquery$1('div.select-styled.active').not(this).each(function () {
-      jquery$1(this).removeClass('active').next('ul.select-options').hide();
-    });
-    jquery$1(this).toggleClass('active').next('ul.select-options').fadeToggle(120);
-  });
-
-  $listItems.click(function (e) {
-    e.stopPropagation();
-    $styledSelect.text(jquery$1(this).text()).removeClass('active');
-    $this.val(jquery$1(this).attr('rel'));
-    $list.hide();
-    //console.log($this.val());
-  });
-
-  jquery$1(document).click(function () {
-    $styledSelect.removeClass('active');
-    $list.hide();
-  });
-});
 //# sourceMappingURL=app.js.map
